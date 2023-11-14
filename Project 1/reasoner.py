1dc6978306c6abe62240f41edee68ff5ccb04858
ChackMociute <martynasvaznonis@gmail.com>
2023-11-14 21:50:34 +0100

    Moved the implementation to a class

--- Project 1/reasoner.py
+++ Project 1/reasoner.py
@@ -1,11 +1,13 @@
 import os
 from sys import argv
+from collections import namedtuple
+from collections.abc import Sequence
 from py4j.java_gateway import JavaGateway
 
 argv.extend(['potato_bowls.ttl', 'CheesyBowl'])
 
 if len(argv) < 3:
-    raise SyntaxError(f"""Missing ontology file and/or class name. Should call as:
+    raise SyntaxError(f"""Missing ontology file and/or class name. Should call with:
                       \tpython {os.path.basename(__file__)} ONTOLOGY_FILE CLASS_NAME""")
 
 gateway = JavaGateway()
@@ -15,23 +17,53 @@ ontology = gateway.getOWLParser().parseFile(argv[1] if os.path.exists(argv[1]) e
 # Change all conjunctions so that they have at most two conjuncts
 gateway.convertToBinaryConjunctions(ontology)
 
-axioms = ontology.tbox().getAxioms()
-elf = gateway.getELFactory()
 
-nodes = {0: [elf.getConjunction(elf.getConceptName(argv[2]), elf.getConceptName('A'))]}
-changed = True
+Relation = namedtuple('relation', 'relation node')
 
-def update_nodes():
-    changed = False
-    for n, concepts in nodes.items():
-        for concept in concepts:
-            if concept.getClass().getSimpleName() == 'ConceptConjunction':
-                for c in concept.getConjuncts():
-                    if c not in concepts:
-                        concepts.append(c)
+class Node(Sequence):
+    def __init__(self, name, concepts=list(), relations=list()):
+        self.name = name
+        self.concepts = concepts
+        self.relations = relations
+    
+    def __getitem__(self, i):
+        return self.concepts[i]
+    
+    def __len__(self):
+        return len(self.concepts)
+    
+    def append(self, item):
+        self.concepts.append(item)
 
-while changed:
-    changed = update_nodes()
+class ELReasoner:
+    def __init__(self, ontology):
+        self.ontology = ontology
+        self.axioms = ontology.tbox().getAxioms()
+        self.elf = gateway.getELFactory()
 
-for v in nodes[0]:
+    def find_subsumers(self, class_name):
+        self.nodes = [Node(0, [self.elf.getConjunction(self.elf.getConceptName(class_name), self.elf.getConceptName('A'))])]
+        # self.nodes = [Node(0, [self.elf.getConceptName(class_name)])]
+        self.changed = True
+        while self.changed:
+            self.changed = False
+            self.update_nodes()
+
+    def update_nodes(self):
+        for node in self.nodes:
+            self.conjunction_rule1(node)
+
+    def conjunction_rule1(self, node):
+        # Add all concepts in conjunctions that are not currently part of the node
+        for c in [conjunct for c in node
+                  if c.getClass().getSimpleName() == 'ConceptConjunction'
+                  for conjunct in c.getConjuncts()]:
+            if c not in node:
+                node.append(c)
+                self.changed = True
+
+reasoner = ELReasoner(ontology)
+reasoner.find_subsumers(argv[2])
+
+for v in reasoner.nodes[0]:
     print(formatter.format(v))
